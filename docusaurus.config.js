8a4b10e855ed91bac5b3e7e1b30da912069e7228
Lộc Nguyễn <97457272+vilo0411@users.noreply.github.com>
2025-03-18 06:27:45 +0700

    Fix bug image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://vilo0411.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Tarot-Project',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
