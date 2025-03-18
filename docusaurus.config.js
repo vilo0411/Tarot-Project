9f743eff6de8054b437634bbc5709929e7dadd94
Lộc Nguyễn <97457272+vilo0411@users.noreply.github.com>
2025-03-18 07:21:12 +0700

    update editURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/vilo0411/Tarot-Project/edit/main/',
         },
         blog: {
           showReadingTime: true,
